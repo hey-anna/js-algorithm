dad94c28c9d9ad6d67a604e7a5df5c18ad106064
hey-anna <annawp@naver.com>
2025-07-01 15:56:15 +0900

    feat: [baekjoon] 1920번 문제 정리 및 풀이 이해
    - 백준 1920번 '수 찾기' 문제 풀이 이해 중심으로 학습
    - 알고리즘 입문 단계
    - 문제 이해에 시간이 다소 걸림
    - 다양한 풀이 참고해 로직과 동작 방식 파악에 집중

---
+++ baekjoon/1920_find-number/1920_v1_map-return.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const A = input[1].split(" ").map(Number); // A: 탐색 대상 배열
+const B = input[3].split(" ").map(Number); // B: 찾고 싶은 숫자들
+
+A.sort((a, b) => a - b); // 이진 탐색을 위한 정렬
+
+let answer = [];
+
+B.forEach((v) => {
+  let start = 0;
+  let end = A.length - 1;
+  let found = false;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+
+    if (v < A[mid]) {
+      end = mid - 1;
+    } else if (v > A[mid]) {
+      start = mid + 1;
+    } else {
+      found = true;
+      break;
+    }
+  }
+
+  answer.push(found ? 1 : 0);
+});
+
+console.log(answer.join("\n"));
